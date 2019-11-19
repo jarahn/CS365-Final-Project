86f827dbdc6247836645cf3f1f9289dc232b2184
jarahn <jarahn@nmu.edu>
2019-11-19 16:02:34 -0500

    Mongo

--- pub/client.js
+++ pub/client.js
@@ -8,3 +8,17 @@ $( document ).ready(function() {
 });
 
 var socket = io();
+
+
+socket.on("setBookList", function(bookList) {
+    $("#theBookList").html("");
+    for(let book of bookList) {
+        var tdLink = $("<td></td>").text(book.Link);
+
+        var tr = $("<tr></tr>").append(tdLink);
+
+        $("#theBookList").append(tr);
+    }
+
+    console.log(bookList);
+});
