05b6b381458f498e6613900a948fabb748cdbcb6
jarahn <jarahn@nmu.edu>
2019-11-19 15:21:50 -0500

    Mongo stuff

--- pub/client.js
+++ pub/client.js
@@ -1,1 +1,3 @@
 //Will contain all client side code
+
+var socket = io();
---
+++ server.js
@@ -0,0 +1,28 @@
+//Stuff for MongoDB
+var mongodb = require("mongodb");
+var MongoClient = mongodb.MongoClient;
+var ObjectID = mongodb.ObjectID;
+var client = new MongoClient("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true });
+var db;
+
+//Stuff for express, server, and socket.io
+var express = require("express");
+var app = express();
+var http = require("http");
+var server = http.Server(app);
+var socketio = require("socket.io");
+var io = socketio(server);
+app.use(express.static("pub"));
+
+
+//If we want a button for the user that will display all available books we can use this function to get them from the DB
+function sendAllBooksToClient(theSocket) {
+	db.collection("Book List").find({}, {sort: [['title', 1]]}).toArray(function(error, documents) {
+		if (error != null) {
+			console.log(error);
+		}
+		else {
+			theSocket.emit("setBookList", documents);
+		}
+	});
+}
