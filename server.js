e1e0e9c1e761ae26849d67f389e511446d0eaebe
jarahn <jarahn@nmu.edu>
2019-11-19 16:52:55 -0500

    Server updates

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Stuff for MongoDB
 var mongodb = require("mongodb");
 var MongoClient = mongodb.MongoClient;
-var ObjectID = mongodb.ObjectID;
 var client = new MongoClient("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true });
 var db;
 
@@ -27,9 +26,31 @@ function sendAllBooksToClient(theSocket) {
 	});
 }
 
+io.on("connection", function(socket) {
+	console.log("Somebody connected.");
 
-socket.on("getBooks", function() {
-    console.log("Got call to getBooks");
-    // talk to the database and get the list of books, send it back to the client.
-    sendAllBooksToClient(socket);
+	socket.on("disconnect", function() {
+		//This particular socket connection was terminated (probably the client went to a different page or closed their browser).
+		console.log("Somebody disconnected.");
+	});
+
+	socket.on("getBooks", function() {
+		console.log("Got call to getBooks");
+		// talk to the database and get the list of books, send it back to the client.
+		sendAllBooksToClient(socket);
+	});
+
+});
+
+
+client.connect(function(err) {
+	if (err != null) throw err; //No DB connection?  Then let our server crash with an error.
+	else {
+		db = client.db("bookStore"); //Get our specific database
+
+		//Start listening for client connections
+		server.listen(80, function() {
+			console.log("Server with socket.io is ready.");
+		});
+	}
 });
